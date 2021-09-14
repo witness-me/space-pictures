b3ad4ff502f1b75bbfe854a3273465840872e6bb
Vladislav Nikolaenko <vladislavnikolaenko@gmail.com>
2021-09-13 21:21:23 -0700

    add basic favorite picture gallery

--- src/views/MyFavorites.vue
+++ src/views/MyFavorites.vue
@@ -1,5 +1,12 @@
 <template>
   <v-container>
     <h1>This is Favorites page</h1>
+    <v-row>
+      <v-col v-for="(item, i) in $store.getters.getFavorites" :key="i" cols="12" sm="6" lg="4">
+        <v-card>
+          <img :src="item" style="width: 100%" alt="" />
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
